e00396e5fbea1069dc3b4accf3f06367d2e97e6d
enzojpn <enzo_jpn@hotmail.com>
2024-04-06 15:52:46 -0300

    implementando os testes TDD

--- CalculadoraTest/CalculadoraTest.cs
+++ CalculadoraTest/CalculadoraTest.cs
@@ -3,36 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using Calculadora;
 
 namespace CalculadoraTest
 {
     public class CalculadoraTest
-    { 
+    {
+
+        private CalculadoraImp _calculadora;
+        public CalculadoraTest()
+        {
+            _calculadora = new CalculadoraImp();
+        }
+
         [Fact]
         public void Somar5Com10Retorna15()
         {
-            Assert.True(false);
+            int resultado = _calculadora.soma(5, 10);
+
+            Assert.Equal(15, resultado);
         }
 
         [Fact]
         public void Subtrair5de10Retorna5()
         {
-            Assert.True(false);
+            int resultado = _calculadora.subtracao(10, 5);
+
+            Assert.Equal(5, resultado);
         }
+
         [Fact]
         public void Multiplicar5por2Retornar10()
         {
-            Assert.True(false);
+            int resultado = _calculadora.multiplicacao(2, 5);
+
+            Assert.Equal(10, resultado);
         }
+
         [Fact]
         public void Dividir10por2Retorna5()
         {
-            Assert.True(false);
+            int resultado = _calculadora.subtracao(10, 2);
+
+            Assert.Equal(5, resultado);
         }
+
         [Fact]
         public void AoFazer3OperacoesConsultarHistoricoDevoverListaCom3elementos()
         {
-            Assert.True(false);
+            var listaHistorticoResultados = _calculadora.historicoResultados();
+
+            Assert.Equal(3, listaHistorticoResultados.Count);
+
         }
     }
 }
